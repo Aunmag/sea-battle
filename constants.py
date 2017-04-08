096f09c2a281d989912f33e82c4afadf50c97ba6
Aunmag <aunmag@gmail.com>
2017-04-08 16:20:22 +0500

    Updated constans

--- constants.py
+++ constants.py
@@ -11,17 +11,21 @@ CELL_SPACE_EMPTY = '.'
 CELL_SPACE_HIT = '*'
 CELL_SPACE_BUFFER = '^'
 
-X_AXIS_DIRECTED = 0
-Y_AXIS_DIRECTED = 1
-
 OFFSETS = (-1, 0, 1)
 
-HIT_STATUS_UNKNOWN = -2
-HIT_STATUS_MISS_REPEATED = -1
-HIT_STATUS_MISS = 0
-HIT_STATUS_DAMAGED = 1
-HIT_STATUS_DESTROYED = 2
+
+class AxisDirection(Enum):
+    X = 0
+    Y = 1
 
 
-class CONSOLE(Enum):
+class HitStatus(Enum):
+    UNKNOWN = 0
+    MISS = 1
+    MISS_REPEATED = 2
+    DAMAGED = 3
+    DESTROYED = 4
+
+
+class Console(Enum):
     WRONG_INPUT = 0
